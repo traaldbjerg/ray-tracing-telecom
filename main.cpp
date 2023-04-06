de15ea96e5a32786ac19dae1eb8e77a23ef826da
Alexander <alexander.stevins@gmail.com>
2023-04-06 13:34:00 +0200

    small changes

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main() {
     double Ly = 10.0;
     double power = 0.0;
     std::vector<double> t(2); t[0] = -5.0 ; t[1] = 5.0; // coordonnées de l'émetteur
-    int recursion_depth = 2; //nombre de fois qu'on effectue la récursion
+    int recursion_depth = 2; // nombre de fois qu'on effectue la récursion
     std::vector<Wall> layout(4); // vecteur qui contiendra l'ensemble des murs
     // initialiser layout
     layout.push_back(Wall(0.0, 0.0, 10.0, 0.0, 2));
@@ -24,30 +24,31 @@ int main() {
 
     std::vector<double> r(2);
 
-    
+    FILE *f_rays = fopen("rays.dat", "w");
+    FILE *f_power = fopen("power.dat", "w");
 
 
 
 
 
-
-    //fonctions à appeler
+    // fonctions à appeler
 
     for (int i = 0; i <= Lx * h; i++) {
         r[0] = i/h;
         for(int j = 0 ; j < Ly * h ; j++) {
             r[1] = j/h;
             for (int k = 1; k < recursion_depth; k++) { // d'abord transmission directe, puis 1 interaction puis 2 etc
-            compute_reflections(layout, t, r, k, power);
+            compute_reflections(*f_rays, layout, t, r, k, power);
             }
         }
     }
 
+    fclose(f_rays);
+    fclose(f_power);
 
 
 
-
-    //résultats à sortir (gnuplot?)
+    // résultats à sortir (gnuplot?)
 
 
 
--- reflections.cpp
+++ reflections.cpp
@@ -1,6 +1,6 @@
 #include "reflections.hpp"
 
-std::vector<double> compute_reflections(std::vector<Wall> layout, std::vector<double> t, std::vector<double> r, int rec_depth, double &d) {
+std::vector<double> compute_reflections(FILE f, std::vector<Wall> layout, std::vector<double> t, std::vector<double> r, int rec_depth, double &d) {
     // layout ensemble des murs du plan ; r les coordonnées du récepteur
     // trouver les n-uples de murs qui permettent les réflexions successives jusqu'au récepteur
     // retourne un vecteur de double, les 2 premières composantes sont les coordonnées de l'émetteur virtuel et le 3e est la perte de puissance
@@ -25,24 +25,20 @@ std::vector<double> compute_reflections(std::vector<Wall> layout, std::vector<do
             double dist = (diff[1] * (r[0] - layout[i].getU()[0]) - diff[0] * (r[1] - layout[i].getU()[1])) / (layout[i].getW()[0] * diff[1] - layout[i].getW()[1] * diff[0]);
                                                                                                             // division par 0 ssi rayon parallèle au mur
 
-            if (0.0 < dist && dist < 1.0) { // sinon la réflexion est invalide car en dehors du mur -> pas besoin de calculer plus
+            if (0.0 < dist && dist < 1.0) { // sinon la réflexion est invalide car en dehors du mur -> pas besoin de calculer plus de choses
 
                 // calculer la perte de puissance ici -> laisser une méthode de Wall donner les coefs de réflexion et de transmission?
 
-                /*double add_dist = 0.0;
-                add_dist += (r[0] - t_virtuel[0]) * (r[0] - t_virtuel[0]);
-                add_dist += (r[1] - t_virtuel[1]) * (r[1] - t_virtuel[1]);
-                add_dist = sqrt(add_dist);
-                d += add_dist;*/
+                
                 if (rec_depth > 1) {
-                    compute_reflections(layout, t_virtuel, r, rec_depth - 1, d); //
+                    compute_reflections(f, layout, t_virtuel, r, rec_depth - 1, d); //
                 }
             }
         }
         else { // transmission
 
             // calculer la perte de puissance ici
-            if (rec_depth > 1) compute_reflections(layout, t, r, rec_depth - 1, d);
+            if (rec_depth > 1) compute_reflections(f, layout, t, r, rec_depth - 1, d);
         }
     }
 }
