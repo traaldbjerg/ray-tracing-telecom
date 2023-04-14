90d0d85f957a824bdf125ad23e2b692131669378
Alexander <alexander.stevins@gmail.com>
2023-04-14 12:12:24 +0200

    switched the copy order, reflections work!

--- reflections.cpp
+++ reflections.cpp
@@ -40,15 +40,16 @@ void compute_reflections(FILE f, std::vector<Wall> layout, int previous_wall_ind
 
             std::cout << "t_virtuel " << i + 1 << ": (" << t_virtuel[0] << ", " << t_virtuel[1] << ")" << std::endl;
 
-            std::vector<double> r_copy = r; // copie de r pour la récursion qui peut être modifiée par l'itération suivante
+            //std::vector<double> r_copy = r; // copie de r pour la récursion qui peut être modifiée par l'itération suivante
                 
             if (rec_depth > 1) {
-                compute_reflections(f, layout, i, t_virtuel, r_copy, rec_depth - 1, d, rays_in_scope); // récursion
+                compute_reflections(f, layout, i, t_virtuel, r, rec_depth - 1, d, rays_in_scope); // récursion
             } else {
                 Ray new_ray;
-                rays_in_scope.push_back(new_ray);
                 new_ray.extend_path(r);
                 new_ray.remove_placeholder_point(); // on peut retirer le placeholder car le vrai récepteur est maintenant connu
+                rays_in_scope.push_back(new_ray); // très important de mettre cette commande après le changement de new_ray, sinon la copie pas modifiée est fournie à la suite
+                //std::cout << "this is a new ray" << std::endl; new_ray.print_path();
             }
 
             std::cout << "rays_in_scope size: " << rays_in_scope.size() << std::endl;
