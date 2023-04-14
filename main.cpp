52a96f2550ab64a31fd1098de2d7ea6155f11d10
Alexander <alexander.stevins@gmail.com>
2023-04-14 14:01:17 +0200

    the ray portion should be fully functional

--- main.cpp
+++ main.cpp
@@ -13,8 +13,9 @@ int main() {
     //double Lx = 10.0;
     //double Ly = 10.0;
     double power = 0.0;
-    std::vector<double> t(2); t[0] = -5.0 ; t[1] = 5.0; // coordonnées de l'émetteur
-    int recursion_depth = 10; // nombre de fois qu'on effectue la récursion
+    std::vector<double> t(2); t[0] = 32 ; t[1] = 10; // coordonnées de l'émetteur
+    std::vector<double> r(2); r[0] = 47; r[1] = 65; // coordonnées du récepteur
+    int recursion_depth = 2; // nombre de fois qu'on effectue la récursion
     std::vector<Wall> layout; // vecteur qui contiendra l'ensemble des murs
     std::vector<Ray> rays; // vecteur qui contiendra l'ensemble des rayons
     // initialiser layout
@@ -24,16 +25,15 @@ int main() {
     //layout.emplace_back(0.0, 10.0, 10.0, 10.0, 2);
     //layout.emplace_back(10.0, 0.0, 10.0, 10.0, 2);
     //std::cout << "hello x2" << std::endl;
-    Wall wall1(0.0, 0.0, 10.0, 0.0, 2); layout.push_back(wall1);
-    Wall wall2(0.0, 0.0, 0.0, 10.0, 2); layout.push_back(wall2);
-    Wall wall3(0.0, 10.0, 10.0, 10.0, 2); layout.push_back(wall3);
-    Wall wall4(10.0, 0.0, 10.0, 10.0, 2); layout.push_back(wall4);
+    // disposition de l'exo 8.1
+    Wall wall1(0.0, 0.0, 0.0, 80.0, 2); layout.push_back(wall1);
+    Wall wall2(0.0, 20.0, 80.0, 20.0, 2); layout.push_back(wall2);
+    Wall wall3(0.0, 80.0, 80.0, 80.0, 2); layout.push_back(wall3);
+    //Wall wall4(10.0, 0.0, 10.0, 10.0, 2); layout.push_back(wall4);
+    //Wall wall5(4, 3, 6, 5, 2); layout.push_back(wall5);
     //std::cout << "hello x3" << std::endl;
     
-    std::vector<double> r(2); // coordonées du récepteur, changera pour chaque itération de la boucle ci-dessous
-
-    r[0] = 5.0; r[1] = 5.0; // coordonnées du récepteur
-
+    
     //int q = layout.size(); // nombre de murs
 
     FILE *f_rays = fopen("rays.dat", "w");
@@ -54,11 +54,11 @@ int main() {
 
     for (int k = recursion_depth; k > 0; k--) { // d'abord max de réflexions puis ... puis 1 interaction puis 0 etc
         compute_reflections(*f_rays, layout, 100, t, r, k, power, rays); // 100 en placeholder, fonctionne tant qu'il y a moins de 100 murs
-
-        // vérifier quels rayons sont acceptables, et les tracer
-        
-        
     }
+
+    // vérifier quels rayons sont acceptables, et les tracer
+
+    Ray direct_ray(r); direct_ray.extend_path(t); rays.push_back(direct_ray); // rajouter le rayon direct
 
     for (int i = 0; i < rays.size(); i++) {
         rays[i].extend_path(t); // rajouter l'émetteur à la liste des points du rayon
